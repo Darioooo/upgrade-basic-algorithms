3b42e22f4cef05485e756ef6538d86a4f5831ec1
Darioooo <darionietosastre@gmail.com>
2023-10-20 11:00:19 +0200

    iteracion_1

---
+++ iteracion_1.js
@@ -0,0 +1,18 @@
+myFavouriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+const gato = {
+    nombre : 'misifu',
+    años : 3,
+    color: 'blanco',
+    horarios: {
+        mañana: 8,
+        tarde: 2,
+        noche: 9
+    }
+}
+
+console.log("suma", gato.horarios.mañana + gato.horarios.tarde)
